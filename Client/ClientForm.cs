6ca6f5b7008bdaf18942a6fae65f7ed0d5df6510
Not Secret <sashaskvorhevskiy@gmail.com>
2023-05-28 00:50:02 +0300

    Client part implementation file

---
+++ Client/ClientForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+
+namespace NetStat
+{
+    namespace Client
+    {
+        public partial class ClientForm : Form
+        {
+            /// <summary>
+            /// Сокет клиента.
+            /// </summary>
+            private Socket client;
+
+            /// <summary>
+            /// Хранит состояние клиента. 
+            /// True - подключен к серверу. False - не подключен к серверу.
+            /// </summary>
+            private bool isClientConnected = false;
+
+            /// <summary>
+            /// Поток входных данных.
+            /// </summary>
+            private MemoryStream inputStream = new MemoryStream();
+
+            /// <summary>
+            /// Поток выходных данных.
+            /// </summary>
+            private MemoryStream outputStream = new MemoryStream();
+
+            /// <summary>
+            /// Преобразователь в бинарный вид.
+            /// </summary>
+            private readonly BinaryFormatter formatter = new BinaryFormatter();
+
+            /// <summary>
+            /// Конструктор по умолчанию.
+            /// </summary>
+            public ClientForm()
+            {
+                InitializeComponent();
+            }
+
+            /// <summary>
+            /// Callback-функция, реагирует на нажатие кнопки.
+            /// Переключает состояние клиента на «Подключен» и «Не подключен».
+            /// </summary>
+            /// <param name="sender">Объект, который вызвал функцию.</param>
+            /// <param name="e">
+            /// Параметр, ссылающийся на базовый класс для классов, содержащих данные событий, и предоставляет
+            /// значение для событий, не содержащих данных.
+            /// </param>
+            private async void OnToggleServerClick(object sender, EventArgs e)
+            {
+                try
+                {
+                    // Если клиент не подключен к серверу
+                    if (!isClientConnected)
+                    {
+                        // Инициализируем сокет клиента
+                        client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        // Асинхронно подключаемся к серверу
+                        await client.ConnectAsync(ipAddressText.Text, int.Parse(portText.Text));
+
+                        // Если удалось подключиться
+                        if (client.Connected)
+                        {
+                            clientLogs.Items.Add($"Подключение к серверу {client.RemoteEndPoint} успешно установлено!");
+                            btnToggleConnection.Text = "Отключиться";
+                            btnGetData.Enabled = true;
+                            isClientConnected = true;
+                            ipAddressText.Enabled = false;
+                            portText.Enabled = false;
+                        }
+
+                        return;
+                    }
+
+                    // Если клииент подключен к серверу
+                    if (isClientConnected)
+                    {
+                        clientLogs.Items.Add($"Отключение от сервера");
+                        btnToggleConnection.Text = "Подключиться";
+                        btnGetData.Enabled = false;
+                        isClientConnected = false;
+                        ipAddressText.Enabled = true;
+                        portText.Enabled = true;
+
+                        client.Shutdown(SocketShutdown.Both);
+                        client.Close();
+
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            /// <summary>
+            /// Callback-функция, реагирует на нажатие кнопки.
+            /// Получает данные с сервера и выводит результат в форму.
+            /// </summary>
+            /// <param name="sender">Объект, который вызвал функцию.</param>
+            /// <param name="e">
+            /// Параметр, ссылающийся на базовый класс для классов, содержащих данные событий, и предоставляет
+            /// значение для событий, не содержащих данных.
+            /// </param>
+            private async void OnGetDataClick(object sender, EventArgs e)
+            {
+                try
+                {
+                    // Для начала отправим серверу объект NetStatObject
+                    // Инициализируем объект
+                    NetStatObject netStatObject = new NetStatObject
+                    {
+                        RequestList = RequestList.GET_TCP_DATA
+                    };
+
+                    // Сериализуем его в поток байтов
+                    formatter.Serialize(outputStream, netStatObject);
+                    // Записываем получившийся поток байтов в переменную
+                    byte[] netStatObjectBytes = outputStream.ToArray();
+
+                    // Отправляем объект на сервер в виде массива байтов, асинхронно
+                    _ = await client.SendAsync(new ArraySegment<byte>(netStatObjectBytes), SocketFlags.None);
+                    // Сообщаем об этом пользователю
+                    clientLogs.Items.Add($"На сервер был отправлен запрос: {netStatObject.RequestList}");
+
+                    // Получаем объект от сервера
+                    // Подготавливаем массив байтов для хранения объектв
+                    byte[] receivedBytes = new byte[client.ReceiveBufferSize];
+                    // Асинхронно получаем объект в виде байтом с сервера
+                    int receivedBytesRead = await client.ReceiveAsync(new ArraySegment<byte>(receivedBytes), SocketFlags.None);
+                    // Инициализируем потом байтов изходя из полученных данных
+                    inputStream = new MemoryStream(receivedBytes, 0, receivedBytesRead)
+                    {
+                        // Позиция в потоке
+                        Position = 0
+                    };
+                    // Десериализируем и преобразовываем в объект
+                    NetStatObject responceNetStatObject = (NetStatObject)formatter.Deserialize(inputStream);
+                    // Уведомляем об этом пользователя
+                    clientLogs.Items.Add($"Получено сообщение от сервера: {responceNetStatObject.Message}");
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
