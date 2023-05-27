06fc59e76738392eeb1448848c9f4abe0f37c991
Not Secret <sashaskvorhevskiy@gmail.com>
2023-05-28 00:47:49 +0300

    Main ClientForm Files

---
+++ Client/ClientForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace NetStat
+{
+    namespace Client
+    {
+        partial class ClientForm
+        {
+            /// <summary>
+            /// Обязательная переменная конструктора.
+            /// </summary>
+            private System.ComponentModel.IContainer components = null;
+
+            /// <summary>
+            /// Освободить все используемые ресурсы.
+            /// </summary>
+            /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+
+            #region Код, автоматически созданный конструктором форм Windows
+
+            /// <summary>
+            /// Требуемый метод для поддержки конструктора — не изменяйте 
+            /// содержимое этого метода с помощью редактора кода.
+            /// </summary>
+            private void InitializeComponent()
+            {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClientForm));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ipAddressText = new System.Windows.Forms.TextBox();
+            this.portText = new System.Windows.Forms.TextBox();
+            this.clientLogs = new System.Windows.Forms.ListBox();
+            this.btnToggleConnection = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 276F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnGetData, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.clientLogs, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnToggleConnection, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 84F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 53F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 56F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(904, 441);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnGetData.Enabled = false;
+            this.btnGetData.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGetData.Location = new System.Drawing.Point(3, 388);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(270, 50);
+            this.btnGetData.TabIndex = 5;
+            this.btnGetData.Text = "Получить данные";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.OnGetDataClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 78);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Подключение";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.2653F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 61.7347F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.ipAddressText, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.portText, 1, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(264, 59);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Адрес:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Порт:";
+            // 
+            // ipAddressText
+            // 
+            this.ipAddressText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.ipAddressText.Location = new System.Drawing.Point(104, 4);
+            this.ipAddressText.Name = "ipAddressText";
+            this.ipAddressText.Size = new System.Drawing.Size(157, 20);
+            this.ipAddressText.TabIndex = 2;
+            this.ipAddressText.Text = "127.0.0.1";
+            this.ipAddressText.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // portText
+            // 
+            this.portText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.portText.Location = new System.Drawing.Point(104, 34);
+            this.portText.Name = "portText";
+            this.portText.Size = new System.Drawing.Size(157, 20);
+            this.portText.TabIndex = 3;
+            this.portText.Text = "8000";
+            this.portText.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // clientLogs
+            // 
+            this.clientLogs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.clientLogs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clientLogs.FormattingEnabled = true;
+            this.clientLogs.IntegralHeight = false;
+            this.clientLogs.ItemHeight = 16;
+            this.clientLogs.Location = new System.Drawing.Point(279, 3);
+            this.clientLogs.Name = "clientLogs";
+            this.tableLayoutPanel1.SetRowSpan(this.clientLogs, 4);
+            this.clientLogs.Size = new System.Drawing.Size(622, 435);
+            this.clientLogs.TabIndex = 2;
+            // 
+            // btnToggleConnection
+            // 
+            this.btnToggleConnection.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnToggleConnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnToggleConnection.Location = new System.Drawing.Point(3, 335);
+            this.btnToggleConnection.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.btnToggleConnection.Name = "btnToggleConnection";
+            this.btnToggleConnection.Size = new System.Drawing.Size(270, 50);
+            this.btnToggleConnection.TabIndex = 1;
+            this.btnToggleConnection.Text = "Подключиться";
+            this.btnToggleConnection.UseVisualStyleBackColor = true;
+            this.btnToggleConnection.Click += new System.EventHandler(this.OnToggleServerClick);
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 441);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(920, 480);
+            this.MinimumSize = new System.Drawing.Size(690, 360);
+            this.Name = "ClientForm";
+            this.Text = "Клиент";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+            }
+
+            #endregion
+
+            private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+            private System.Windows.Forms.GroupBox groupBox1;
+            private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+            private System.Windows.Forms.Label label1;
+            private System.Windows.Forms.Label label2;
+            private System.Windows.Forms.TextBox ipAddressText;
+            private System.Windows.Forms.TextBox portText;
+            private System.Windows.Forms.ListBox clientLogs;
+            private System.Windows.Forms.Button btnToggleConnection;
+            private System.Windows.Forms.Button btnGetData;
+        }
+    }
+}
