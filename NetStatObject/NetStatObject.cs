22cd2abc0388d006d7b58934f2c76ed55eadee2f
Not Secret <sashaskvorhevskiy@gmail.com>
2023-05-28 00:49:45 +0300

    Class for transfer between client and server

---
+++ NetStatObject/NetStatObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NetStat
+{
+    [Serializable]
+    public class NetStatObject 
+    {
+        public string Message { get; set; }
+        public RequestList RequestList { get; set; }
+
+        public NetStatObject() 
+        {
+            RequestList = RequestList.GET_TCP_DATA;
+        }
+    }
+}
