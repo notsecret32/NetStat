fd9133a9f9158ee93418075270b1e4440627087c
Not Secret <sashaskvorhevskiy@gmail.com>
2023-05-29 23:27:02 +0300

    The object to be transferred has been supplemented

--- NetStatObject/NetStatObject.cs
+++ NetStatObject/NetStatObject.cs
@@ -6,11 +6,21 @@ namespace NetStat
     public class NetStatObject 
     {
         public string Message { get; set; }
+        public string IPAddress { get; set; }
+        public int Port { get; set; }
+        public int BytesReveived { get; set; }
+        public int BytesSended { get; set; }
+        public string ProtocolType { get; set; }
         public RequestList RequestList { get; set; }
 
         public NetStatObject() 
         {
             RequestList = RequestList.GET_TCP_DATA;
+            IPAddress   = string.Empty;
+            Port        = 0;
+            BytesReveived = 0;
+            BytesSended   = 0;
+            ProtocolType  = string.Empty;
         }
     }
 }
